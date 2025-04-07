b9a1bbaac999462a21f904df1d5cb53e967c88f2
firozrahisuddinsaifi <firozkhandelhi62@gmail.com>
2025-04-07 13:21:08 +0530

    responsive code added

--- src/Components/FoodOptions.jsx
+++ src/Components/FoodOptions.jsx
@@ -10,12 +10,15 @@ export default function FoodOptions(){
 
     return(<>
 
-        <div className="w-[85%] container mx-auto flex flex-wrap mt-20 gap-3">
+<div className="w-[80%] container mx-auto flex flex-wrap mt-20 gap-3 px-4 sm:px-6 md:px-8 lg:px-10">
 
-            {
-                imageGridCards?.map((foodData)=><FoodCard key={foodData.id} foodData={foodData}/>)
-                
-            }
-        </div>
+{
+    imageGridCards?.map((foodData) => (
+        <FoodCard key={foodData.id} foodData={foodData} />
+    ))
+
+}
+</div>
+
     </>)
 }
