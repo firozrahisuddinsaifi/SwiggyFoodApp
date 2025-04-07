fe19c0c3f8f65009bfe40a82bab17e92eec53711
firozrahisuddinsaifi <firozkhandelhi62@gmail.com>
2025-04-07 21:41:31 +0530

    done changes

--- src/Components/RestInfo.jsx
+++ src/Components/RestInfo.jsx
@@ -1,11 +1,36 @@
 import { useState } from "react"
 import { ImageURL } from "../Utils/Images"
+import { addItems,incrementItems,decrementItems } from "../Stored/CartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
 export default function RestInfo({restData}){
 
+    
+    // const [count,setCount]=useState(0);
 
+    const dispatch = useDispatch();
+    const items = useSelector(state=>state.cartslice.items)
+    const element = items.find(item=>item.id===restData.id)
+
+    const count = element?element.quantity:0;
+    console.log("count value ",count)
+
+    function handleAddItems(){
+        // setCount(1);
+        dispatch(addItems(restData));
+    }
+
+    function handleIncrementItems(){
+        // setCount(count+1);
+        dispatch(incrementItems(restData))
+    }
+
+    function handleDecrementItems(){
+        // setCount(count-1);
+        dispatch(decrementItems(restData));
+    }
     const [isExpanded, setIsExpanded]=useState(false);
 
     function toggleVisibility(){
@@ -29,6 +54,20 @@ return(<>
           <button onClick={toggleVisibility} className="mt-2 text-gray-500 font-bold">{isExpanded ? "Show Less" : "Show More"}</button>
   </div>
 
+    {/*** food items image and add button*/}
+    <div className="w-[20%] relative rounded-2xl h-45 ">
+        <img className="w-full h-36 object-cover rounded-3xl" src={ImageURL.imageBaseURL+restData.imageId}></img>
+       
+       {
+        count===0?(<button className="absolute bottom-4 left-18 border-white shadow-md px-6 py-2 bg-white text-green-700 rounded-xl hover:cursor-pointer" onClick={()=>handleAddItems()}>ADD </button>
+        ):(<div className="flex absolute bottom-4 left-18 gap-2  px-6 py-2 bg-white text-green-700 rounded-xl border-white shadow-md ">
+            <button className="hover:cursor-pointer" onClick={()=>handleDecrementItems()}>-</button>
+            <span>{count}</span>
+            <button className="hover:cursor-pointer" onClick={()=>handleIncrementItems()}>+</button>
+        </div>)
+       }
+
+    </div>
   {/*** food items image and add button */}
   <div className=" md:w-[20%] relative rounded-2xl h-45">
     <img
